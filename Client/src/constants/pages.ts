a7632e556934f1c36a91c1603e87f53d1085df67
Cims <Cims@gmail.com>
2026-03-31 17:59:06 +0000

    Latest Build for Copi Data Check

--- Client/src/constants/pages.ts
+++ Client/src/constants/pages.ts
@@ -1,6 +1,6 @@
 // Innovace Intech Solution Pvt Ltd
 // ⚠️ AUTO-GENERATED FILE – DO NOT EDIT
-// Generated on 2026-03-31T17:28:56.227Z
+// Generated on 2026-03-31T17:58:17.145Z
 
 export const pages = [
   {
