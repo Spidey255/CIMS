ddee6baa0a55d25e76071c52f99124e1abb10ba5
Cims <Cims@gmail.com>
2026-03-31 16:51:27 +0000

    Combo load issue in Repeater Grid

--- Client/src/constants/pages.ts
+++ Client/src/constants/pages.ts
@@ -1,6 +1,6 @@
 // Innovace Intech Solution Pvt Ltd
 // ⚠️ AUTO-GENERATED FILE – DO NOT EDIT
-// Generated on 2026-03-31T14:21:21.550Z
+// Generated on 2026-03-31T16:50:34.879Z
 
 export const pages = [
   {
