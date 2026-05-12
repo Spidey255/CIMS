e18a2bdd0273368e6466a93a01305af9162e3107
Innovace <srinivash.g@innovaceintech.com>
2026-05-12 15:12:09 +0530

    spent details

--- DynamicDFSAPI/iProofProcessService/StartUp.cs
+++ DynamicDFSAPI/iProofProcessService/StartUp.cs
@@ -43,12 +43,12 @@ namespace CPS.Proof.DFSExtension
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
-                    Title = "CommitmentSpentDetails",
+                    Title = "ProjectFundInstallment",
                     Description = "Test",
 
                     Contact = new OpenApiContact
                     {
-                        Name = "CommitmentSpentDetails",
+                        Name = "ProjectFundInstallment",
                     },
                     License = new OpenApiLicense
                     {
@@ -89,8 +89,8 @@ namespace CPS.Proof.DFSExtension
                 app.UseSwaggerUI();
                 app.UseSwaggerUI(c =>
                 {
-                    c.DocumentTitle = "CommitmentSpentDetails";
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommitmentSpentDetails");
+                    c.DocumentTitle = "ProjectFundInstallment";
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjectFundInstallment");
                     c.DisplayOperationId();
                     c.DisplayRequestDuration();
                     c.DocExpansion(DocExpansion.List);
