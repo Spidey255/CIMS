00dc8d9ff49bf4cf1c49df6671216793b6916c91
Innovace <srinivash.g@innovaceintech.com>
2026-05-13 22:59:16 +0530

    Fund Installment Save Included

--- Client/src/constants/pages.ts
+++ Client/src/constants/pages.ts
@@ -1,6 +1,6 @@
 // Innovace Intech Solution Pvt Ltd
 // ⚠️ AUTO-GENERATED FILE – DO NOT EDIT
-// Generated on 2026-05-13T14:24:03.749Z
+// Generated on 2026-05-13T17:26:33.273Z
 
 export const pages = [
   {
