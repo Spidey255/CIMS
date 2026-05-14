a2eaa2039a079f7be6bc5030765542fc5378569f
Innovace <srinivash.g@innovaceintech.com>
2026-05-14 21:22:43 +0530

    spent details

--- Client/src/constants/pages.ts
+++ Client/src/constants/pages.ts
@@ -1,6 +1,6 @@
 // Innovace Intech Solution Pvt Ltd
 // ⚠️ AUTO-GENERATED FILE – DO NOT EDIT
-// Generated on 2026-05-14T15:24:36.854Z
+// Generated on 2026-05-14T15:49:26.179Z
 
 export const pages = [
   {
