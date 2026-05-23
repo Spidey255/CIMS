40e885037d407c0087ccf4cabc5a856061d70fb8
Cims <Cims@gmail.com>
2026-05-23 10:21:16 +0000

    Build 1

--- Client/src/constants/pages.ts
+++ Client/src/constants/pages.ts
@@ -1,8 +1,14 @@
 // Innovace Intech Solution Pvt Ltd
 // ⚠️ AUTO-GENERATED FILE – DO NOT EDIT
-// Generated on 2026-05-23T09:35:07.046Z
+// Generated on 2026-05-23T10:19:55.554Z
 
 export const pages = [
+  {
+    "PackageProcessMapId": "64C42232-67DB-43B8-8B68-4468FE0DD17F",
+    "ProcessActivityMapId": "2B40E332-9840-42E3-8DDC-002EAFAA05FA",
+    "ProcessName": "ProjectFundInstallment",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectFundInstallment_AdditionalDetails"
+  },
   {
     "PackageProcessMapId": "B219A0EA-0254-4F69-B989-B681DD475183",
     "ProcessActivityMapId": "34CF8FAF-7574-478F-9C4A-A65F98E4D18A",
@@ -10,16 +16,10 @@ export const pages = [
     "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectsExplorer_Start"
   },
   {
-    "PackageProcessMapId": "B1037B85-D9C1-411B-87DE-0658365B81A8",
-    "ProcessActivityMapId": "788CA62E-DF0F-4071-B3E3-4239827AFC52",
-    "ProcessName": "ProjectReimbursement",
-    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectReimbursement_PIInitiation"
-  },
-  {
-    "PackageProcessMapId": "026e321e-9bb7-4c74-8615-3bbd7cc1b241",
-    "ProcessActivityMapId": "B3A49337-4C4B-42EC-9F9B-1C4C8B4E2E13",
-    "ProcessName": "ProjectProposal",
-    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectProposal_Start"
+    "PackageProcessMapId": "6B8D5C61-726C-4E9B-96C6-9D4269CAB215",
+    "ProcessActivityMapId": "D0B6EBB9-10A4-4D43-A76E-F39441A41EF6",
+    "ProcessName": "ProjectCreation",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectCreation_Initiation"
   },
   {
     "PackageProcessMapId": "9A6DB1E6-872F-4A0C-B21C-6BE13EC6E295",
@@ -28,28 +28,10 @@ export const pages = [
     "RoutPath": "PRJ20251020000000025-R&DProjects/TempAdvance_PIInitiation"
   },
   {
-    "PackageProcessMapId": "C12B87A0-C375-4057-B0BA-64B4C80E77F9",
-    "ProcessActivityMapId": "C74E1FE0-A9E3-4B09-A184-6D1DC9CE7B0F",
-    "ProcessName": "CommitmentSpentDetails",
-    "RoutPath": "PRJ20251020000000025-R&DProjects/CommitmentSpentDetails_Start"
-  },
-  {
-    "PackageProcessMapId": "84777088-B51C-45AA-8BBA-D24FBDEF26E9",
-    "ProcessActivityMapId": "4E51124D-BF83-46EC-BA9A-B746693F716F",
-    "ProcessName": "FundConfigurations",
-    "RoutPath": "PRJ20251020000000025-R&DProjects/FundConfigurations_Start"
-  },
-  {
-    "PackageProcessMapId": "64C42232-67DB-43B8-8B68-4468FE0DD17F",
-    "ProcessActivityMapId": "DDBF5292-6EF4-4C37-B72A-E6DCFFBF7B02",
-    "ProcessName": "ProjectFundInstallment",
-    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectFundInstallment_PIInitiation"
-  },
-  {
-    "PackageProcessMapId": "654C8734-EE30-4F33-B278-659E77BFDD71",
-    "ProcessActivityMapId": "AAB05128-7AAE-4A99-8278-465822CE1E85",
-    "ProcessName": "TravelRequest",
-    "RoutPath": "PRJ20251020000000025-R&DProjects/TravelRequest_PIInitiation"
+    "PackageProcessMapId": "026e321e-9bb7-4c74-8615-3bbd7cc1b241",
+    "ProcessActivityMapId": "B3A49337-4C4B-42EC-9F9B-1C4C8B4E2E13",
+    "ProcessName": "ProjectProposal",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectProposal_Start"
   },
   {
     "PackageProcessMapId": "AC69E996-4AE4-4D34-829F-4DFC7F63582C",
@@ -58,9 +40,51 @@ export const pages = [
     "RoutPath": "PRJ20251020000000025-R&DProjects/TravelSettlement_PIInitiation"
   },
   {
-    "PackageProcessMapId": "6B8D5C61-726C-4E9B-96C6-9D4269CAB215",
-    "ProcessActivityMapId": "D0B6EBB9-10A4-4D43-A76E-F39441A41EF6",
-    "ProcessName": "ProjectCreation",
-    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectCreation_Initiation"
+    "PackageProcessMapId": "64C42232-67DB-43B8-8B68-4468FE0DD17F",
+    "ProcessActivityMapId": "F38AD8C8-6832-4FC5-A52D-7EB3B0FEE9F8",
+    "ProcessName": "ProjectFundInstallment",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectFundInstallment_View0"
+  },
+  {
+    "PackageProcessMapId": "654C8734-EE30-4F33-B278-659E77BFDD71",
+    "ProcessActivityMapId": "AAB05128-7AAE-4A99-8278-465822CE1E85",
+    "ProcessName": "TravelRequest",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/TravelRequest_PIInitiation"
+  },
+  {
+    "PackageProcessMapId": "026e321e-9bb7-4c74-8615-3bbd7cc1b241",
+    "ProcessActivityMapId": "F692D026-2B48-418C-9285-DBD42DAC5F26",
+    "ProcessName": "ProjectProposal",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectProposal_ReadOnly"
+  },
+  {
+    "PackageProcessMapId": "84777088-B51C-45AA-8BBA-D24FBDEF26E9",
+    "ProcessActivityMapId": "4E51124D-BF83-46EC-BA9A-B746693F716F",
+    "ProcessName": "FundConfigurations",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/FundConfigurations_Start"
+  },
+  {
+    "PackageProcessMapId": "B1037B85-D9C1-411B-87DE-0658365B81A8",
+    "ProcessActivityMapId": "788CA62E-DF0F-4071-B3E3-4239827AFC52",
+    "ProcessName": "ProjectReimbursement",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectReimbursement_PIInitiation"
+  },
+  {
+    "PackageProcessMapId": "C12B87A0-C375-4057-B0BA-64B4C80E77F9",
+    "ProcessActivityMapId": "C74E1FE0-A9E3-4B09-A184-6D1DC9CE7B0F",
+    "ProcessName": "CommitmentSpentDetails",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/CommitmentSpentDetails_Start"
+  },
+  {
+    "PackageProcessMapId": "64C42232-67DB-43B8-8B68-4468FE0DD17F",
+    "ProcessActivityMapId": "FF2ED876-20AB-4F5D-B62B-385405DFB900",
+    "ProcessName": "ProjectFundInstallment",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectFundInstallment_RDOffice"
+  },
+  {
+    "PackageProcessMapId": "64C42232-67DB-43B8-8B68-4468FE0DD17F",
+    "ProcessActivityMapId": "DDBF5292-6EF4-4C37-B72A-E6DCFFBF7B02",
+    "ProcessName": "ProjectFundInstallment",
+    "RoutPath": "PRJ20251020000000025-R&DProjects/ProjectFundInstallment_PIInitiation"
   }
 ];
