3b704066583f761d70dad3a8208ad1f97243c364
Cims <Cims@gmail.com>
2026-06-03 14:11:48 +0000

    latest build

--- Client/src/constants/pages.ts
+++ Client/src/constants/pages.ts
@@ -1,6 +1,6 @@
 // Innovace Intech Solution Pvt Ltd
 // ⚠️ AUTO-GENERATED FILE – DO NOT EDIT
-// Generated on 2026-06-03T10:16:15.861Z
+// Generated on 2026-06-03T14:10:14.667Z
 
 export const pages = [
   {
