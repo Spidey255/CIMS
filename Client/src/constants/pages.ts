ca82b1fd1f4ab70db5cf8846ad417d533d750e08
Cims <Cims@gmail.com>
2026-07-09 08:10:30 +0000

    ioki

--- Client/src/constants/pages.ts
+++ Client/src/constants/pages.ts
@@ -1,6 +1,6 @@
 // Innovace Intech Solution Pvt Ltd
 // ⚠️ AUTO-GENERATED FILE – DO NOT EDIT
-// Generated on 2026-07-09T08:06:37.891Z
+// Generated on 2026-07-09T08:09:26.923Z
 
 export const pages = [
   {
